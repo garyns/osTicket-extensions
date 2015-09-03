bc9f223be80b500fe9b6aa21e05ba7df2a937f20
root <root@exchange.lynwbl.local>
2015-09-03 12:26:52 +1000

    New Tag Processor

---
+++ hashtag/tag-SUBJECT.php
@@ -0,0 +1,11 @@
+<?php
+  // These variables are available: $link (mysql link), $ticketId, $threadId, $hashtag, $body, $body2 (body with hashtag removed and html stripped);
+  // These functions are available: update_thread($ticketId, $threadId, 'text')  post_ticket_note($ticketId, 'text');
+
+  // TicketStatus is a custom column added in OSTicket.
+
+  $sql = "UPDATE ost_ticket__cdata SET subject='$body2' WHERE ticket_id=$ticketId;";
+  $result = mysql_query($sql, $link);
+  echo $result ? "OK" : "ERROR: " . mysql_error($link);
+  
+  update_thread($ticketId, $threadId, "<strike>$hashtag</strike>\nStatus Updated \'$body2\'");
